ad8edd3eca025216f1a0dc936ee2ff4cd240d51f
Kiet1618 <ngokiet3207@gmail.com>
2022-10-02 00:50:25 +0700

    Kiet1618
    ReactJS

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -6,10 +6,10 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+        <h1>Đây là Web đọc truyện tranh</h1>
+        <p>Đây là trang chủ trang web</p>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
+          <li><a href='https://get.asp.net/'>Nettruyen</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
         </ul>
