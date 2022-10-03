92544b35ebe61d2c1e1b72a492494b7c24123d92
Kiet1618 <ngokiet3207@gmail.com>
2022-10-03 08:28:14 +0700

    Update from K1618
    Reactj

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -9,13 +9,13 @@ export class Home extends Component {
         <h1>Đây là Web đọc truyện tranh</h1>
         <p>Đây là trang chủ trang web</p>
         <ul>
-          <li><a href='https://get.asp.net/'>Nettruyen</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          <li>Truyện đang hot</li>
+          <li>Truyện mới cập nhật</li>
+          <li>Truyện mới trong danh sách theo giỏi.</li>
         </ul>
-        <p>To help you get started, we have also set up:</p>
+        <p>Các bình luận:</p>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
+          <li><strong>Nguyễn Văn A</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
           <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
           <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class Login extends Component {
+  static displayName = Login.name;
+
+  render() {
+    return(<p>Đây là trang đăng nhập.</p>);
+  }
+
+}
